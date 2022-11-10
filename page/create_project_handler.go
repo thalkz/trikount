3b22aab9a53976d343cc9fdb314f5c632fa31a43
Roland Lamidieu <roland.lamidieu@gmail.com>
2022-11-10 14:06:01 +0100

    feat: Implement create project

--- page/create_project_handler.go
+++ page/create_project_handler.go
@@ -3,8 +3,13 @@ package page
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"github.com/thalkz/trikount/database"
+	"github.com/thalkz/trikount/error_helper"
+	"github.com/thalkz/trikount/hash"
 )
 
 func CreateProject(c *gin.Context) {
@@ -13,8 +18,32 @@ func CreateProject(c *gin.Context) {
 	if projectName == "" {
 		c.HTML(http.StatusOK, "create_project.html", nil)
 	} else {
-		// TODO Register project
-		projectId := "PROJECT_ID"
+		projectId, err := findAvailableProjectId()
+		if err != nil {
+			error_helper.HTML(http.StatusInternalServerError, err, c)
+		}
+
+		err = database.CreateProject(projectId, projectName, time.Now())
+		if err != nil {
+			error_helper.HTML(http.StatusInternalServerError, err, c)
+		}
+
 		c.Redirect(http.StatusFound, fmt.Sprintf("/%s", projectId))
 	}
+}
+
+func findAvailableProjectId() (string, error) {
+	maxRetries := 5
+	projectIdLength := 6
+	for retry := 0; retry < 5; retry++ {
+		projectId := hash.NewHash(projectIdLength)
+		exists, err := database.CheckExists(projectId)
+		if err != nil {
+			return "", errors.Wrap(err, "failed to check if project id exists")
+		}
+		if !exists {
+			return projectId, nil
+		}
+	}
+	return "", fmt.Errorf("could not find available project id after %v retries", maxRetries)
 }
